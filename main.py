edaaae00fba7454b393e6787a5f2cc0b4e36829e
Suraj chauhan <surajssc1232@gmail.com>
2024-10-29 21:34:01 +0530

    made a simple llm chat bot

---
+++ main.py
@@ -0,0 +1,34 @@
+import cohere
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key=os.getenv('api_key')
+
+co = cohere.ClientV2("api_key")
+
+def chat_with_cohere():
+    print("Animebot is ready! Ask me anything about any anime but just not MHA its bad. Type 'exit' to end the conversation.")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'exit':
+            print("Chatbot: Goodbye!")
+            break
+
+        response = co.chat(
+            model="command-r",
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_input
+                }
+            ]
+        )
+
+        
+        assistant_message = response.message.content[0].text
+        print(f"Chatbot: {assistant_message}")
+
+if __name__ == "__main__":
+    chat_with_cohere()
